fd9bc7bcd51c9f67e31a6a8b381e6b6cd20e9909
yuriyk <yuriyk@netvertise.com>
2017-05-03 14:11:09 +0300

    Add write to file

--- decode_webpage2.py
+++ decode_webpage2.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 base_url = 'http://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, "lxml")
-
-
-for i in soup.find_all("p", {"data-reactid": re.compile("[2-3][0-9][0-9]")}):
-    if i.string:
-        print(i.string)
+filename = raw_input("Enter output file name: ")
+with open(filename, 'w') as open_file:
+    for i in soup.find_all("p", {"data-reactid": re.compile("[2-3][0-9][0-9]")}):
+        if i.string:
+            open_file.write(str(i.string)+'\n')
